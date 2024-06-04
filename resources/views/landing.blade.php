9fbc0a4be460c67d1402c87d671f06823114e07e
mel <romelcub@gmail.com>
2024-06-04 15:27:06 +0800

    Updates : Tue, Jun  4, 2024  3:27:06 PM

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -9,7 +9,8 @@
         {{ $title }}
     </title>
     @include('sections.styles')
-    <link rel="stylesheet" href="{{ asset('css/style.v1.css') }}"></link>
+    <link rel="stylesheet" href="{{ asset('css/style.v1.css') }}">
+    </link>
 </head>
 
 <body>
@@ -20,20 +21,22 @@
                     <div class="container-fluid px-4 py-5 my-5 text-center">
                         <div class="lc-block mb-4">
                             <div editable="rich">
-                                <h2 class="display-2 fw-bold text-light">Showcase <span class="text-primary">your pet!</span></h2>
+                                <h2 class="display-2 fw-bold text-light">Showcase <span class="text-primary">your
+                                        pet!</span></h2>
                             </div>
                         </div>
                         <div class="lc-block col-lg-6 mx-auto mb-5">
                             <div editable="rich">
                                 <p class="lead text-light">
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat facilis ex cum provident ipsa dolore sapiente nemo enim, hic magni officiis excepturi inventore eos unde odio, pariatur saepe tenetur blanditiis.
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat facilis ex cum
+                                    provident ipsa dolore sapiente nemo enim, hic magni officiis excepturi inventore eos
+                                    unde odio, pariatur saepe tenetur blanditiis.
                                 </p>
                             </div>
                         </div>
 
                         <div class="lc-block d-grid gap-2 d-sm-flex justify-content-sm-center mb-5">
-                            <a
-                                class="btn btn-primary btn-lg px-4 gap-3" href="#" role="button">
+                            <a class="btn btn-primary btn-lg px-4 gap-3" href="#" role="button">
                                 Pre-register
                             </a>
                             <a class="btn btn-outline-light btn-lg px-4" href="#" role="button">
@@ -49,71 +52,89 @@
                 </div>
             </div>
         </div>
-        <div class="container py-5">
+        <div class="container">
             <div class="row">
-                <div class="col-12">
+                <div class="col-12 py-5">
                     <div class="lead">
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste praesentium mollitia voluptatem est consequatur veritatis qui quis aut placeat, saepe obcaecati minima necessitatibus similique dolores eius, nostrum architecto, nemo amet!
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste praesentium mollitia voluptatem
+                        est consequatur veritatis qui quis aut placeat, saepe obcaecati minima necessitatibus similique
+                        dolores eius, nostrum architecto, nemo amet!
                     </div>
                 </div>
 
-                <div class="col-12">
+                <div class="col-12 py-5">
                     <div class="lead">
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste praesentium mollitia voluptatem est consequatur veritatis qui quis aut placeat, saepe obcaecati minima necessitatibus similique dolores eius, nostrum architecto, nemo amet!
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste praesentium mollitia voluptatem
+                        est consequatur veritatis qui quis aut placeat, saepe obcaecati minima necessitatibus similique
+                        dolores eius, nostrum architecto, nemo amet!
                     </div>
                 </div>
 
-                <div class="col-12">
+                <div class="col-12 py-5">
                     <div class="lead">
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste praesentium mollitia voluptatem est consequatur veritatis qui quis aut placeat, saepe obcaecati minima necessitatibus similique dolores eius, nostrum architecto, nemo amet!
-                    </div>
-                </div>
-
-
-
-                <div class="col-12">
-                    <div class="lead">
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste praesentium mollitia voluptatem est consequatur veritatis qui quis aut placeat, saepe obcaecati minima necessitatibus similique dolores eius, nostrum architecto, nemo amet!
-                    </div>
-                </div>
-
-
-
-                <div class="col-12">
-                    <div class="lead">
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste praesentium mollitia voluptatem est consequatur veritatis qui quis aut placeat, saepe obcaecati minima necessitatibus similique dolores eius, nostrum architecto, nemo amet!
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste praesentium mollitia voluptatem
+                        est consequatur veritatis qui quis aut placeat, saepe obcaecati minima necessitatibus similique
+                        dolores eius, nostrum architecto, nemo amet!
                     </div>
                 </div>
 
 
 
-                <div class="col-12">
+                <div class="col-12 py-5">
                     <div class="lead">
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste praesentium mollitia voluptatem est consequatur veritatis qui quis aut placeat, saepe obcaecati minima necessitatibus similique dolores eius, nostrum architecto, nemo amet!
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste praesentium mollitia voluptatem
+                        est consequatur veritatis qui quis aut placeat, saepe obcaecati minima necessitatibus similique
+                        dolores eius, nostrum architecto, nemo amet!
+                    </div>
+                </div>
+
+
+
+                <div class="col-12 py-5">
+                    <div class="lead">
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste praesentium mollitia voluptatem
+                        est consequatur veritatis qui quis aut placeat, saepe obcaecati minima necessitatibus similique
+                        dolores eius, nostrum architecto, nemo amet!
+                    </div>
+                </div>
+
+
+
+                <div class="col-12 py-5">
+                    <div class="lead">
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste praesentium mollitia voluptatem
+                        est consequatur veritatis qui quis aut placeat, saepe obcaecati minima necessitatibus similique
+                        dolores eius, nostrum architecto, nemo amet!
                     </div>
                 </div>
 
 
 
 
-                <div class="col-12">
+                <div class="col-12 py-5">
                     <div class="lead">
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste praesentium mollitia voluptatem est consequatur veritatis qui quis aut placeat, saepe obcaecati minima necessitatibus similique dolores eius, nostrum architecto, nemo amet!
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste praesentium mollitia voluptatem
+                        est consequatur veritatis qui quis aut placeat, saepe obcaecati minima necessitatibus similique
+                        dolores eius, nostrum architecto, nemo amet!
                     </div>
                 </div>
 
 
 
-                <div class="col-12">
+                <div class="col-12 py-5">
                     <div class="lead">
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste praesentium mollitia voluptatem est consequatur veritatis qui quis aut placeat, saepe obcaecati minima necessitatibus similique dolores eius, nostrum architecto, nemo amet!
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste praesentium mollitia voluptatem
+                        est consequatur veritatis qui quis aut placeat, saepe obcaecati minima necessitatibus similique
+                        dolores eius, nostrum architecto, nemo amet!
                     </div>
                 </div>
 
 
-                <div class="col-12">
+                <div class="col-12 py-5">
                     <div class="lead">
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste praesentium mollitia voluptatem est consequatur veritatis qui quis aut placeat, saepe obcaecati minima necessitatibus similique dolores eius, nostrum architecto, nemo amet!
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste praesentium mollitia voluptatem
+                        est consequatur veritatis qui quis aut placeat, saepe obcaecati minima necessitatibus similique
+                        dolores eius, nostrum architecto, nemo amet!
                     </div>
                 </div>
 
@@ -123,11 +144,13 @@
     <div class="footer">
         <div class="container">
             <div class="row">
-                <div class="col-12 col-lg-6">
+                <div class="col-12 col-lg-6 py-5">
 
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime sunt a, porro commodi aliquid beatae dolorem. Accusamus dolores temporibus alias unde quae voluptas optio minus cupiditate illo fugiat, voluptatem soluta?
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime sunt a, porro commodi aliquid beatae
+                    dolorem. Accusamus dolores temporibus alias unde quae voluptas optio minus cupiditate illo fugiat,
+                    voluptatem soluta?
                 </div>
-                <div class="col-12 col-lg-6">
+                <div class="col-12 col-lg-6 py-5">
 
                     <ul class="list-group list-group-numbered">
                         <li class="list-group-item active">Active item</li>
@@ -136,6 +159,11 @@
                     </ul>
 
 
+                </div>
+                <div class="col-12 text-center py-5">
+                    <small>
+                        Copyright @2024
+                    </small>
                 </div>
             </div>
         </div>
